8a469b48e475e5999c95350154a1c010910c1217
codeSalim <abrarbitu@gmail.com>
2024-02-03 21:49:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Facebook from './componet/facebook'
+
+function App() {
+return (<>
+
+  <Facebook></Facebook>
+
+
+</>)}
+export default App
